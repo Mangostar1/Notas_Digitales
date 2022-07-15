83a6e12bbbc107e7f28543ebe3c52893d8d02cd7
Omar <93488662+Mangostar1@users.noreply.github.com>
2022-07-15 13:33:54 -0400

    Se guardan cambios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, {useEffect, useState} from 'react'
 
 export default function App() {
+    const [nav, setNav] = useState(0)
+
     return(
-        <header>
-            <div>Logo</div>
-            <nav>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        </header>
+        <>
+            <header>
+                <div>Logo</div>
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                
+            </main>
+        </>
     );
 }
